cbfee928d1c9e04d2a300050087dbd60f669864b
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-06-15 11:28:01 -0700

    Add basic authentication

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -2,19 +2,44 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PostPage from './pages/PostPage';
 import ThemeToggle from './components/ThemeToggle';
-import EditPost from './pages/PostPage';
+import EditPost from './pages/EditPost';
+import LoginPage from './pages/Login';
+import RequireAuth from './components/RequireAuth';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeToggle />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<PostPage />} />
-        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={(
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          )}
+        />
+        <Route
+          path="/post/:id"
+          element={(
+            <RequireAuth>
+              <PostPage />
+            </RequireAuth>
+          )}
+        />
+        <Route
+          path="/edit/:id"
+          element={(
+            <RequireAuth>
+              <EditPost />
+            </RequireAuth>
+          )}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
